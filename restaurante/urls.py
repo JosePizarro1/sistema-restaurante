cb5460d514ceef2b3ef91da095bc6810158e7450
JosePizarro1 <josepizarroarca@gmail.com>
2026-07-26 22:00:12 -0500

    feat: habilitar ruta explicita /reportes/ apuntando a la vista de reportes

--- restaurante/urls.py
+++ restaurante/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('', views.reportes_view, name='reportes'),
+    path('reportes/', views.reportes_view, name='reportes_alias'),
     path('pos/', views.pos_view, name='pos'),
     path('cocina/', views.cocina_view, name='cocina'),
     path('cocina/estado/<int:orden_id>/<str:nuevo_estado>/', views.cambiar_estado_orden, name='cambiar_estado'),
